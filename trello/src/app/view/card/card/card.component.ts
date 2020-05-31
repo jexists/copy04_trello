c7e8e3f13b8ffdb03fd70a0bedfbadb57ecdb0b2
jexists <jexists486@gmail.com>
2020-05-31 11:11:47 +0900

    card layout

--- trello/src/app/view/card/card/card.component.ts
+++ trello/src/app/view/card/card/card.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Card } from '../../../core/models/index';
+
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss']
 })
 export class CardComponent implements OnInit {
-
+  
+  card: Card[];
+  
   constructor() { }
 
   ngOnInit(): void {
