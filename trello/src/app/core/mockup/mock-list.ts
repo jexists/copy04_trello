ef2ec3da54d253f29d24fd1da084c24ba3c888b6
jexists <jexists486@gmail.com>
2021-07-18 20:19:36 +0900

    backup

--- trello/src/app/core/mockup/mock-list.ts
+++ trello/src/app/core/mockup/mock-list.ts
@@ -1,6 +1,6 @@
-import { List } from '../models/index';
-export const LISTS: List[] = [
-  { listUUID: 112, listTitle: 'text123' },
-  { listUUID: 122, listTitle: '테스트용입니다' },
-  { listUUID: 132, listTitle: '테스티 보드이름입니다. 나중에 지울예정' }
-];
+// import { List } from '../models/index';
+// export const LISTS: List[] = [
+//   { listUUID: 112, listTitle: 'text123' },
+//   { listUUID: 122, listTitle: '테스트용입니다' },
+//   { listUUID: 132, listTitle: '테스티 보드이름입니다. 나중에 지울예정' }
+// ];
